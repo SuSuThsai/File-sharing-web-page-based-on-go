a76c2182faa4ce67d2ab9a79cdce0a01910e5e42
SuSuThsai <1210289284@github.com>
2022-05-23 23:21:46 +0800

    全局变量

--- File-sharing-web-page/Global/Global.go
+++ File-sharing-web-page/Global/Global.go
@@ -2,17 +2,19 @@ package Global
 
 import (
 	//本地
-
 	//第三方
+	"github.com/go-redis/redis/v8"
 	"github.com/jakeslee/aliyundrive"
+	"gorm.io/gorm"
 	//目录
 	"File-sharing-web-page/ALiServer/ALiconfig"
-	"File-sharing-web-page/GINServer/UserInfo"
+	"File-sharing-web-page/GINServer/GINconfig"
 )
 
 //全局变量
 
-var ConfigFilePath = "ALiServer/ALiconfig/AliInits.yaml"
+var AliConfigFilePath = "ALiServer/ALiconfig/AliInits.yaml"
+var GINConfigFilePath = "GINServer/GINconfig/GinInits.yaml"
 
 var Global struct {
 	//阿里端
@@ -21,5 +23,11 @@ var Global struct {
 	DriverConfig       ALiconfig.DriverConfig
 	RefreshTokenConfig ALiconfig.RefreshTokenConfig
 	//Gin端
-	User UserInfo.UserInfo
+	User      GINconfig.UserInfo
+	DB        *gorm.DB
+	DBR       *redis.Client
+	DBR2      *redis.Client
+	GINMysql  GINconfig.MysqlInfo
+	GINRedis  GINconfig.RedisInfo
+	GINRedis2 GINconfig.RedisInfo
 }
