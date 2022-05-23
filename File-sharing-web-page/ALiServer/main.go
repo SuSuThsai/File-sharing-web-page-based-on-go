49632bc103617b5b86809dd33aee42d5063df8fd
SuSuThsai <1210289284@github.com>
2022-05-23 23:20:58 +0800

    TEST

--- File-sharing-web-page/ALiServer/main.go
+++ File-sharing-web-page/ALiServer/main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
+	//本地
+	//第三方
+	//目录
 	"File-sharing-web-page/ALiServer/ALiInits"
-	"File-sharing-web-page/ALiServer/UpLoad"
-	"fmt"
-	"os"
 )
 
 //初始化
 func main() {
 	ALiInits.InitsConfig()
 	ALiInits.InitsDriver()
-	file, _ := os.Open("F:\\图片\\奈奈\\奈奈1.jpg")
-	fileId := UpLoad.UpLoad(file)
-	URLInfo := UpLoad.GetUrl(fileId)
-	fmt.Println("", *URLInfo.Url, fileId)
 }
