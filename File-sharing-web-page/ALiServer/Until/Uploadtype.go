82fb709c4515d7886739a0c759b28ba462fa47f9
SuSuThsai <1210289284@github.com>
2022-05-23 23:20:09 +0800

    获得文件类型和上传

---
+++ File-sharing-web-page/ALiServer/Until/Uploadtype.go
@@ -0,0 +1,5 @@
+package Until
+
+func UploadType(x string) string {
+	return "/文件"
+}
--- File-sharing-web-page/ALiServer/UpLoad/UpLoadFile.go
+++ File-sharing-web-page/ALiServer/UpLoad/UpLoadFile.go
@@ -1,44 +1,40 @@
 package UpLoad
 
 import (
-	//本地
-	"fmt"
 	"log"
-	"os"
 	"path"
 	//第三方
 	"github.com/jakeslee/aliyundrive"
+	"mime/multipart"
 	//目录
-	"File-sharing-web-page/ALiServer/ALiInits"
+	"File-sharing-web-page/ALiServer/Until"
 	"File-sharing-web-page/Global"
 )
 
-func UpLoad(f *os.File) (fileId string) {
+func UpLoad(f *multipart.FileHeader) (fileId string) {
 	fullpath := IsExist()
-	stat, _ := os.Stat(f.Name())
-	filetype := ALiInits.UploadType(path.Ext(stat.Name()))
+	f1, _ := f.Open()
+	filetype := Until.UploadType(path.Ext(f.Filename))
 	rsp, _, err := Global.Global.Driver.ResolvePathToFileId(Global.Global.Credential, fullpath+filetype)
 	if err != nil && err != aliyundrive.ErrPartialFoundPath {
-		fmt.Println("寻找路径出错:", err.Error())
-		log.Panic(err)
+		log.Println("寻找路径出错:", err.Error())
 	}
 	file, err := Global.Global.Driver.UploadFile(Global.Global.Credential, &aliyundrive.UploadFileOptions{
-		Name:             stat.Name(),
-		Size:             stat.Size(),
+		Name:             f.Filename,
+		Size:             f.Size,
 		ParentFileId:     rsp,
 		ProgressStart:    nil,
 		ProgressCallback: nil,
 		ProgressDone:     nil,
-		Reader:           f,
+		Reader:           f1,
 	})
 	if err != nil && err != aliyundrive.ErrPartialFoundPath {
-		fmt.Println("上传文件失败:", err.Error())
-		log.Panic(err)
+		log.Println("上传文件失败:", err.Error())
 	}
 	if file == nil {
 		fileId = ""
 	} else {
-		fmt.Println(file.ParentFileId, file.Name)
+		//fmt.Println(file.ParentFileId, file.Name)
 		fileId = file.FileId
 	}
 	return
